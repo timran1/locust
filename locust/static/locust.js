97765b04f07e97532d722ee158f0b04f564a4412
Talha Imran <timran@psu.edu>
2020-12-25 16:51:23 -0700

    Fix 99 percentile

--- locust/static/locust.js
+++ locust/static/locust.js
@@ -192,7 +192,7 @@ function updateStats() {
             var total = report.stats[report.stats.length-1];
             // update charts
             rpsChart.addValue([total.current_rps, total.current_fail_per_sec], report.user_count);
-            responseTimeChart.addValue([report.current_response_time_percentile_50, report.current_response_time_percentile_95], report.user_count, report.current_response_time_percentile_99);
+            responseTimeChart.addValue([report.current_response_time_percentile_50, report.current_response_time_percentile_95, report.current_response_time_percentile_99], report.user_count);
             usersChart.addValue([report.user_count]);
         } else {
             appearStopped();
--- locust/web.py
+++ locust/web.py
@@ -274,6 +274,9 @@ class WebUI:
                 report["total_rps"] = stats[len(stats) - 1]["current_rps"]
                 report["fail_ratio"] = environment.runner.stats.total.fail_ratio
                 report[
+                    "current_response_time_percentile_99"
+                ] = environment.runner.stats.total.get_current_response_time_percentile(0.99)
+                report[
                     "current_response_time_percentile_95"
                 ] = environment.runner.stats.total.get_current_response_time_percentile(0.95)
                 report[
